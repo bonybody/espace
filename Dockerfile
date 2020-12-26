8b9f5109910709b14b1a4dadf02d932a09984c31
bonybody <yumaaa312@gmail.com>
2020-12-26 23:18:38 +0900

    cacheを削除

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM node:14.1.0-alpine
-
-RUN apk add chromium \
-            chromium-chromedriver \
--- app/src/js/first-view.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-  setTimeout(() => {
-    const spinner = document.getElementById('first-view');
-    spinner.classList.add('first-view__loaded');
-  }, 1000)
-}
